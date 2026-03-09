48f8b3770d3447454bc6ec1e9e29f4ea29509040
saran <selvakumarraj432@gmail.com>
2026-03-09 22:03:54 +0530

    [Fix]:Camalcase issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-// import Header from './MainComponent/Header'
 import PiechartContent from './MainComponent/PiechartContent'
-import BrandContent from './MainComponent/brandContent'
-import Navbar from './MainComponent/navbar'
-import Footer from './MainComponent/footer'
-import Testimonials from './MainComponent/testimonials'
-import Features from './MainComponent/features'
-import Pricing from './MainComponent/pricing'
-import FeedbackAns from './MainComponent/feedbackAns'
-import Highlights from './MainComponent/highlights'
+import BrandContent from './MainComponent/BrandContent'
+import Navbar from './MainComponent/Navbar'
+import Footer from './MainComponent/Footer'
+import Testimonials from './MainComponent/Testimonials'
+import Features from './MainComponent/Features'
+import Pricing from './MainComponent/Pricing'
+import FeedbackAns from './MainComponent/FeedbackAns'
+import Highlights from './MainComponent/Highlights'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     {/* <Header/> */}
      <Navbar/>
      <PiechartContent/>
      <BrandContent/>
      <Highlights/>
      <Features/>
      <Testimonials/>
-      <Pricing/>
-      <FeedbackAns/>
+     <Pricing/>
+     <FeedbackAns/>
      <Footer/>
     </>
   )
